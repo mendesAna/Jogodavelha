ddafdc0b5a33df871d8d2b355e75fccc6822ceff
anabe <anabe@uninove.edu.br>
2018-10-25 21:28:48 -0300

    Jogo ok

---
+++ Jogo da velha/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Jogo_da_velha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.botao11 = new System.Windows.Forms.Button();
+            this.botao12 = new System.Windows.Forms.Button();
+            this.botao13 = new System.Windows.Forms.Button();
+            this.botao21 = new System.Windows.Forms.Button();
+            this.botao22 = new System.Windows.Forms.Button();
+            this.botao23 = new System.Windows.Forms.Button();
+            this.botao31 = new System.Windows.Forms.Button();
+            this.botao32 = new System.Windows.Forms.Button();
+            this.botao33 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // botao11
+            // 
+            this.botao11.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.botao11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botao11.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao11.Location = new System.Drawing.Point(12, 42);
+            this.botao11.Name = "botao11";
+            this.botao11.Size = new System.Drawing.Size(58, 58);
+            this.botao11.TabIndex = 0;
+            this.botao11.UseVisualStyleBackColor = false;
+            // 
+            // botao12
+            // 
+            this.botao12.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.botao12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botao12.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao12.Location = new System.Drawing.Point(76, 42);
+            this.botao12.Name = "botao12";
+            this.botao12.Size = new System.Drawing.Size(58, 58);
+            this.botao12.TabIndex = 1;
+            this.botao12.UseVisualStyleBackColor = false;
+            // 
+            // botao13
+            // 
+            this.botao13.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.botao13.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botao13.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao13.Location = new System.Drawing.Point(140, 42);
+            this.botao13.Name = "botao13";
+            this.botao13.Size = new System.Drawing.Size(58, 58);
+            this.botao13.TabIndex = 2;
+            this.botao13.UseVisualStyleBackColor = false;
+            // 
+            // botao21
+            // 
+            this.botao21.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.botao21.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botao21.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao21.Location = new System.Drawing.Point(12, 106);
+            this.botao21.Name = "botao21";
+            this.botao21.Size = new System.Drawing.Size(58, 58);
+            this.botao21.TabIndex = 3;
+            this.botao21.UseVisualStyleBackColor = false;
+            // 
+            // botao22
+            // 
+            this.botao22.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.botao22.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botao22.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao22.Location = new System.Drawing.Point(76, 106);
+            this.botao22.Name = "botao22";
+            this.botao22.Size = new System.Drawing.Size(58, 58);
+            this.botao22.TabIndex = 4;
+            this.botao22.UseVisualStyleBackColor = false;
+            // 
+            // botao23
+            // 
+            this.botao23.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.botao23.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botao23.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao23.Location = new System.Drawing.Point(140, 106);
+            this.botao23.Name = "botao23";
+            this.botao23.Size = new System.Drawing.Size(58, 58);
+            this.botao23.TabIndex = 5;
+            this.botao23.UseVisualStyleBackColor = false;
+            // 
+            // botao31
+            // 
+            this.botao31.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.botao31.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botao31.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao31.Location = new System.Drawing.Point(11, 170);
+            this.botao31.Name = "botao31";
+            this.botao31.Size = new System.Drawing.Size(58, 58);
+            this.botao31.TabIndex = 6;
+            this.botao31.UseVisualStyleBackColor = false;
+            // 
+            // botao32
+            // 
+            this.botao32.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.botao32.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botao32.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao32.Location = new System.Drawing.Point(76, 170);
+            this.botao32.Name = "botao32";
+            this.botao32.Size = new System.Drawing.Size(58, 58);
+            this.botao32.TabIndex = 7;
+            this.botao32.UseVisualStyleBackColor = false;
+            // 
+            // botao33
+            // 
+            this.botao33.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.botao33.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botao33.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao33.Location = new System.Drawing.Point(140, 170);
+            this.botao33.Name = "botao33";
+            this.botao33.Size = new System.Drawing.Size(58, 58);
+            this.botao33.TabIndex = 8;
+            this.botao33.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Museo Sans For Dell", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label1.Location = new System.Drawing.Point(35, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 22);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "X, é a sua vez";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(210, 240);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.botao33);
+            this.Controls.Add(this.botao32);
+            this.Controls.Add(this.botao31);
+            this.Controls.Add(this.botao23);
+            this.Controls.Add(this.botao22);
+            this.Controls.Add(this.botao21);
+            this.Controls.Add(this.botao13);
+            this.Controls.Add(this.botao12);
+            this.Controls.Add(this.botao11);
+            this.Name = "Form1";
+            this.Text = "Jogo da Velha";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button botao11;
+        private System.Windows.Forms.Button botao12;
+        private System.Windows.Forms.Button botao13;
+        private System.Windows.Forms.Button botao21;
+        private System.Windows.Forms.Button botao22;
+        private System.Windows.Forms.Button botao23;
+        private System.Windows.Forms.Button botao31;
+        private System.Windows.Forms.Button botao32;
+        private System.Windows.Forms.Button botao33;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Jogo da velha/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jogo_da_velha
+{
+    public partial class Form1 : Form
+    {
+        bool xis = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            botao11.Click += new EventHandler(botaoClick);
+            botao12.Click += new EventHandler(botaoClick);
+            botao13.Click += new EventHandler(botaoClick);
+            botao21.Click += new EventHandler(botaoClick);
+            botao22.Click += new EventHandler(botaoClick);
+            botao23.Click += new EventHandler(botaoClick);
+            botao31.Click += new EventHandler(botaoClick);
+            botao32.Click += new EventHandler(botaoClick);
+            botao33.Click += new EventHandler(botaoClick);
+
+            foreach (Control item in this.Controls)
+            {
+                if (item is Button)
+                {
+                    item.TabStop = false;
+                }
+
+            }
+
+        }
+        private void botaoClick(object sende, EventArgs e)
+        {
+            ((Button)sende).Text = this.xis ? "X" : "O";
+            ((Button)sende).Enabled = false;
+
+            VerificarGanhador();
+            xis = !xis;
+
+            label1.Text = string.Format("{0}, é a sua vez", this.xis ? "X" : "O");
+
+        }
+        private void VerificarGanhador()
+        {
+            if (
+                botao11.Text != String.Empty && botao11.Text == botao12.Text && botao12.Text == botao13.Text ||
+                botao21.Text != String.Empty && botao21.Text == botao22.Text && botao22.Text == botao23.Text ||
+                botao31.Text != String.Empty && botao31.Text == botao32.Text && botao32.Text == botao33.Text ||
+
+                botao11.Text != String.Empty && botao11.Text == botao21.Text && botao21.Text == botao31.Text ||
+                botao12.Text != String.Empty && botao12.Text == botao22.Text && botao22.Text == botao32.Text ||
+                botao13.Text != String.Empty && botao13.Text == botao23.Text && botao23.Text == botao33.Text ||
+
+                botao11.Text != String.Empty && botao11.Text == botao22.Text && botao22.Text == botao33.Text ||
+                botao13.Text != String.Empty && botao13.Text == botao22.Text && botao22.Text == botao31.Text 
+               )
+                
+            {
+                MessageBox.Show(String.Format("O ganhador é o {0}", this.xis ? "X" : "O"), "Temos um Ganhador", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                Reiniciar();
+            }
+            else
+            {
+                VerificarEmpate();
+            }
+            }
+            private void VerificarEmpate()
+             {
+            bool todosDesabilitados = true;
+            foreach (Control item in this.Controls)
+            {
+                if (item is Button && item. Enabled)
+                {
+                    todosDesabilitados = false;
+                    break;
+                }
+            }
+               if (todosDesabilitados)
+            {
+                MessageBox.Show(String.Format("Empatou"), "Opps!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Reiniciar();
+            }
+            }
+                private void Reiniciar()
+                {
+                    foreach (Control item in this.Controls)
+                 {
+                     if (item is Button)
+                {
+                    item.Enabled = true;
+                    item.Text = String.Empty;
+                }
+                  }
+
+                  }
+              
+           
+
+        }
+    }
+    
+
+  
+   
+
